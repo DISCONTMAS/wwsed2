d7f37b9eea4d4bf4e6d6f090bf6d584314ab775a
DISCONTMAS <alex12yavnov11@gmail.com>
2024-06-10 17:10:50 +0700

    Добавлен локальный Bootstrap

--- wwsed2/wwsed2/settings.py
+++ wwsed2/wwsed2/settings.py
@@ -126,6 +126,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [BASE_DIR / 'main' / 'templates' / 'src']
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
